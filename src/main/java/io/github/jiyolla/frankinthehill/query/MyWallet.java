1daf304067964e4b8131d72890aea5f92b0ed835
jiyolla <jiyolla0919@gmail.com>
2024-06-23 22:19:42 +0900

    Add MyWallet to query assets and trades

---
+++ src/main/java/io/github/jiyolla/frankinthehill/query/MyWallet.java
@@ -0,0 +1,27 @@
+package io.github.jiyolla.frankinthehill.query;
+
+import com.binance.connector.client.impl.SpotClientImpl;
+import com.binance.connector.client.impl.spot.Trade;
+import com.binance.connector.client.impl.spot.Wallet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
+public class MyWallet implements Query {
+    private final SpotClientImpl spotClientImpl;
+
+    public String getAssets() {
+        Wallet wallet = spotClientImpl.createWallet();
+        return wallet.getUserAsset(null);
+    }
+
+    public String getTrades(String symbol) {
+        Trade trade = spotClientImpl.createTrade();
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("symbol", symbol);
+        return trade.myTrades(parameters);
+    }
+}
--- src/test/java/io/github/jiyolla/frankinthehill/PlayGroundTest.java
+++ src/test/java/io/github/jiyolla/frankinthehill/PlayGroundTest.java
@@ -2,10 +2,13 @@ package io.github.jiyolla.frankinthehill;
 
 import com.binance.connector.client.impl.SpotClientImpl;
 import com.binance.connector.client.impl.spot.Wallet;
+import io.github.jiyolla.frankinthehill.query.MyWallet;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
 
 @Slf4j
 @SpringBootTest
@@ -14,9 +17,16 @@ class PlayGroundTest {
     @Autowired
     private SpotClientImpl spotClientImpl;
 
+    @Autowired
+    private MyWallet myWallet;
+
     @Test
     void test() {
         Wallet wallet = spotClientImpl.createWallet();
+        spotClientImpl.createMarket().exchangeInfo(new HashMap<>());
+
+        log.info(myWallet.getTrades("BTCUSDT"));
+        log.info(myWallet.getAssets());
         log.info(wallet.apiPermission(null));
     }
 }
