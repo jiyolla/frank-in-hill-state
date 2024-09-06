cef175fcf84a8b69fa4df430260fb1e30084dfed
jiyolla <jiyolla0919@gmail.com>
2024-09-06 12:45:22 +0900

    use spring boot scheduling in main application

--- src/main/java/io/github/jiyolla/frankinhillstate/MainApplication.java
+++ src/main/java/io/github/jiyolla/frankinhillstate/MainApplication.java
@@ -1,31 +1,33 @@
 package io.github.jiyolla.frankinhillstate;
 
 import io.github.jiyolla.frankinhillstate.agent.Ta4jSmaAgent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+
+import static java.time.LocalDateTime.now;
 
 @SpringBootApplication
+@EnableScheduling
+@RequiredArgsConstructor
+@Slf4j
 public class MainApplication {
+    private final Ta4jSmaAgent ta4jSmaAgent;
+
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(MainApplication.class, args);
-        Ta4jSmaAgent ta4jSmaAgent = context.getBean(Ta4jSmaAgent.class);
+        SpringApplication.run(MainApplication.class, args);
+    }
 
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
-
-        Runnable task = () -> ta4jSmaAgent.signal(LocalDateTime.now());
-
-        scheduledExecutorService.scheduleWithFixedDelay(task, 0, 1, TimeUnit.SECONDS);
-
-        // Schedule a task to stop the scheduler after 100 seconds
-        // scheduledExecutorService.schedule(() -> {
-        //     System.out.println("Shutting down the scheduler...");
-        //     scheduledExecutorService.shutdown();
-        // }, 100, TimeUnit.SECONDS);
+    @Scheduled(cron = "*/10 * * * * *")
+    private void signalAgent() {
+        LocalDateTime startTime = now();
+        log.info("Agent wakes up at {}", startTime);
+        ta4jSmaAgent.signal(startTime);
+        log.info("Agent goes back to sleep at {}", now());
     }
 }
