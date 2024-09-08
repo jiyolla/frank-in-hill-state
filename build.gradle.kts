2f88d48ae844850f5d4ca43d7e1be832d2d34602
jiyolla <jiyolla0919@gmail.com>
2024-09-08 19:28:48 +0900

    set up jib and docker repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.my.convention)
     alias(libs.plugins.lombok)
     alias(libs.plugins.spring.boot)
+    alias(libs.plugins.jib)
 }
 
 dependencies {
@@ -9,4 +10,17 @@ dependencies {
     implementation(libs.binance.connector)
     implementation(libs.spring.boot.starter)
     implementation(libs.jackson.databind)
+}
+
+jib {
+    from {
+        image = "amazoncorretto:21-alpine3.20"
+    }
+    to {
+        image = "jiyolla/frank-in-hill-state"
+        auth {
+            username = System.getenv("DOCKER_HUB_USERNAME")
+            password = System.getenv("DOCKER_HUB_PASSWORD")
+        }
+    }
 }
